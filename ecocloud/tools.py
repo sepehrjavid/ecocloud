df2495dff3d6964c0bfd5179fb4a4652d6bb2aca
Anand Vasudevan <anandv96@outlook.com>
2021-11-20 17:08:41 +0200

    spec co and power consumption

--- ecocloud/tools.py
+++ ecocloud/tools.py
@@ -1,6 +1,8 @@
 from counselor.models import *
 import pandas as pd
-from ecocloud.settings import CSV_LOCATION
+from counselor.services import Spec
+from ecocloud.settings import CSV_LOCATION, SPEC_POWER
+from counselor.models import Region
 
 
 def load_csv():
@@ -25,3 +27,23 @@ def get_region_rank(regions, current_region) -> list:
     topRegions.sort(key=lambda x: x.co_foot_print * x.pue)
 
     return topRegions[:5]
+
+
+# returns the carbon footprint of the provided specification in the provided region
+# pass the current cpu usage in number of percentage. Default = 50
+def get_spec_co(spec: Spec, region: Region, cpu_usage=50):
+    power = get_spec_power_use(spec, cpu_usage)
+    return power * region.pue * region.co_foot_print
+
+# returns the power usage of the provided specification. The provider is part of the spec
+# pass the current cpu usage in number of percentage. Default = 50
+
+
+def get_spec_power_use(spec: Spec, cpu_usage=50,):
+    spec.stats = SPEC_POWER[spec.provider]
+    cpu = (spec.stats["max_cpu"] - spec.stats["min_cpu"]) * \
+        cpu_usage/100 + spec.stats["min_cpu"]
+    storage = spec.stats["storage"] * spec.storage / 1024
+    memory = spec.stats["memory"] * spec.memory
+
+    return cpu + storage + memory
