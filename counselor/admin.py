5274f0b00b320e29a5940dc08384f09622eb192d
sepehr javid <javid.sepehr77@gmail.com>
2021-11-20 02:24:09 +0200

    start counselor app, add database scheme

--- counselor/admin.py
+++ counselor/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from counselor.models import *
 
-# Register your models here.
+admin.site.register(Region)
+admin.site.register(Service)
--- counselor/models.py
+++ counselor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    name = models.CharField(max_length=200)
+    co_foot_print = models.IntegerField(null=True)
+    pue = models.IntegerField(null=True)
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    available_regions = models.ManyToManyField(Region, blank=True)
